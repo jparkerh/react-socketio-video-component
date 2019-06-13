17d76285cb8f76f479bafb9470847a6e72ce8ba4
Parker Holloway <parkerh@xlnx.xilinx.com>
2019-06-13 15:23:44 -0500

    first working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
+import socketIOClient from "socket.io-client";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const socket = socketIOClient('http://localhost:8080');
+
+class ImgSocket extends Component {
+
+  constructor (props) {
+    super(props)
+    this.canvasRef = React.createRef();
+  }
+
+  shouldComponentUpdate (props) {
+    var image = new Image();
+    image.src = props.imgsrc
+    const ctx = this.canvasRef.current.getContext('2d');
+
+    image.onload = function() {
+      ctx.drawImage(image, 0, 0);
+    }
+    console.log("drawing")
+    console.log(props.imgsrc)
+
+    return true
+  }
+
+  render () {
+    return (
+      <div>
+        <canvas ref={this.canvasRef} width={640} height={480}/>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      response: "hello, demo",
+      response1: "don't change this"
+    };
+
+    socket.on('user_data', (data) => {
+      this.setState({img: data})
+    });
+  };
+
+  render () {
+    return (
+      <div className="App">
+        <ImgSocket imgsrc={this.state.img}></ImgSocket>
+      </div>
+    )
+  }
+
 }
 
 export default App;
